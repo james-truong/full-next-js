2fbe2cca61a5ec38c01e328ddc3d341c98b35b15
James Truong <james.truong@hotmail.com>
2020-01-28 01:09:17 -0500

    added dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -87,9 +87,14 @@
 
 // export default Home
 
+import Link from 'next/link'
+
 const Index = () => (
   <div>
     <h1>Home page</h1>
+    <Link href='/blog'>
+      <a>Blog</a>
+    </Link>
   </div>
 )
 
